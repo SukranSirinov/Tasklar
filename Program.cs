194f3a003509091200d00ae37650e3841bb46cb8
SukranSirinov <sukransirinov0@gmail.com>
2022-03-05 07:29:33 +0400

    Updated

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 128;
+int hasil = 1;
+int i = 0;
+while(hasil<a)
+{
+    hasil *= 2;
+    i++;
+}
+if (hasil==a)
+{
+    Console.WriteLine("2 nin quvvetidir", a,i);
+}
+else
+{
+    Console.WriteLine("2 nin quvveti deyil");
+}
